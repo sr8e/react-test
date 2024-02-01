85cbefe33cf0516b5659ac6b11a08cc516f2331b
sr8e <sr@mell0w-5phere.net>
2024-02-02 08:31:03 +0900

    testing interaction with flask app

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+function LoginLink() {
+  const [link, setLink] = useState("")
+  useEffect(() => {
+    fetch("/auth/login", { "method": "GET" }).then(
+      res => res.text()
+    ).then(
+      text => setLink(text)
+    )
+  }, [])
+  return <a href={link}>Log in with Google</a>
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <LoginLink />
       </header>
     </div>
   );
